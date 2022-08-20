e485859307adac248d5a96185db10b82a9c68f08
lomekc <lomekc@mail.ru>
2022-08-20 13:15:44 +0300

    добавлен код

--- Program.cs
+++ Program.cs
@@ -1,2 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string[] words(string[] array)
+{
+
+    string[] NewWords = new string[11];
+
+    int count = 0;
+
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i].Length <= 3)
+        {
+            NewWords[count] = array[i];
+            count++;
+        }
+    }
+    Array.Resize(ref NewWords, count);
+    return NewWords;
+}
+
+void Print(string[] array)
+{
+    Console.Write("[" + string.Join(", ", array) + "]");
+    Console.WriteLine();
+}
+
+string[] NewArray = { "hello", "2", "world", ":-)", "1234", "1567", "-2", "computer science", "Russia", "Denmark", "Kazan" };
+
+Print(words(NewArray));
